fa3f14279d481b75e733a6b36a10c53cd105f815
sorhawell <sorhawell@gmail.com>
2015-12-27 10:14:33 +0100

    print.forestFloor_multiClass in separate file

--- R/forestFloor_randomForest_multiClass.R
+++ R/forestFloor_randomForest_multiClass.R
@@ -129,10 +129,3 @@ with randomForest")
 }
 
 
-print.forestFloor_multiClass = function(x,...) {
-  cat("this is a forestFloor_multiClass object \n
-      this object can be plotted in 2D with plot(x), see help(plot.forestFloor) \n
-      this object can be plotted in 3D with show3d(x), see help(show3d) \n
-      \n
-      x contains following internal elements: \n ",with(x,ls()))
-}
---
+++ R/print.forestFloor_multiClass.R
@@ -0,0 +1,9 @@
+
+print.forestFloor_multiClass = function(x,...) {
+  cat("this is a forestFloor_multiClass object \n
+      this object can be plotted in 2D with plot(x), see help(plot.forestFloor) \n
+      this object can be plotted in 3D with show3d(x), see help(show3d) \n
+      this object can be plotted in 3D with show3d(x), see help(plot_simplex3) \n
+      \n
+      x contains following internal elements: \n ",with(x,ls()))
+}
