dd1715670bad99e8c1557069cf0b5b958b33c222
sorhawell <sorhawell@gmail.com>
2016-02-16 14:24:04 +0100

    function to decide if train or test set or both should be used in plotting

---
+++ R/checkPlotTest.R
@@ -0,0 +1,25 @@
+checkPlotTest = function(plotTest,isTrain) {
+  if(is.null(plotTest)) { #if there is a test set plot that as default, else train
+    plotThese = if(all(isTrain)) isTrain else !isTrain
+  } else {
+    matchArg = pmatch(plotTest,c(T,F,"andTrain"))
+    if(length(matchArg)<1 || is.na(matchArg)) {
+      stop(paste("plotTest= '",plotTest,"' is an incorrect argument"))
+    }
+    
+    #if plotTest is T, plot test set if there is one, else raise error
+    if(matchArg==1) plotThese = if(!all(isTrain)) !isTrain else {
+      stop("no test set found, did you pass a Xtest when computing forestFloor?")
+    }
+    
+    #if not to plot test set, then plot train, train should also be there
+    if(matchArg==2) plotThese = isTrain
+    
+    #if plotTest is "andTrain" blot both train and test set
+    if(matchArg==3) {
+      plotThese = rep(T,length(isTrain))
+      if(all(isTrain)) warning("no test found, only plotting train")
+    }
+  }
+return(plotThese)
+}
