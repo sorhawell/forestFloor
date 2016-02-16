13f081bbae2cdb38293f0a4706c2882e644c291d
sorhawell <sorhawell@gmail.com>
2016-02-17 00:25:03 +0100

    test fixes

--- tests/allExamples.R
+++ tests/allExamples.R
@@ -1,15 +1,15 @@
-if(Sys.getenv("USERNAME")=="sorhawell" || Sys.getenv("USER")=="travis") {
+if(Sys.getenv("USERNAME")=="SQHW2" || Sys.getenv("USER")=="travis2") {
   if(!interactive()) Sys.setenv(RGL_USE_NULL=TRUE) #disable RGL for headless machines
   library(devtools)
   library(tools)
-  myPackage = "../forestFloor"
+  myPackage = "./forestFloor"
   
   #remake of devtools::run_examples
   run_examples_simple_no_Roxygenize = function(
     pkg = ".",
     topics = NULL,
     run = TRUE
-    ) {
+  ) {
     pkg = as.package(pkg)
     if(is.null(topics)) {
       files = devtools:::rd_files(pkg)
@@ -28,7 +28,9 @@ if(Sys.getenv("USERNAME")=="sorhawell" || Sys.getenv("USER")=="travis") {
   }
   
   #if a package check, jump out of test fold and into check source folder
-  if(!interactive()) setwd("../00_pkg_src")
+  pa = getwd()
+  if(!interactive()) setwd("../00_pkg_src") else setwd("../.")
   
   run_examples_simple_no_Roxygenize(pkg=myPackage, run=FALSE) #override dontrun
+  setwd(pa)
 }
