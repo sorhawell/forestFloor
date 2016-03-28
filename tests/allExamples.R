2303964b612bfbb2cec56a9cbb6470adb93895f0
sorhawell <sorhawell@gmail.com>
2016-03-28 18:44:54 +0200

    disable allExamples

--- tests/allExamples.R
+++ tests/allExamples.R
@@ -1,36 +1,36 @@
-if(Sys.getenv("USERNAME")=="SQHW2" || Sys.getenv("USER")=="travis2") {
-  if(!interactive()) Sys.setenv(RGL_USE_NULL=TRUE) #disable RGL for headless machines
-  library(devtools)
-  library(tools)
-  myPackage = "./forestFloor"
-  
-  #remake of devtools::run_examples
-  run_examples_simple_no_Roxygenize = function(
-    pkg = ".",
-    topics = NULL,
-    run = TRUE
-  ) {
-    pkg = as.package(pkg)
-    if(is.null(topics)) {
-      files = devtools:::rd_files(pkg)
-    } else {
-      files = unlist(lapply(topics,function(i) {
-        fileName = devtools:::find_pkg_topic(topic=i)
-        if(is.null(fileName)) stop(paste("no .Rd-file match found for[",i,"] when looking up topics"))
-        return(fileName)
-      }))
-      path_man <- file.path(pkg$path, "man")
-      files = paste0(path_man,"/",files)
-      names(files) = basename(files)
-    }
-    lapply(files, devtools:::run_example,run=run)
-    "Done"
-  }
-  
-  #if a package check, jump out of test fold and into check source folder
-  pa = getwd()
-  if(!interactive()) setwd("../00_pkg_src") else setwd("../.")
-  
-  run_examples_simple_no_Roxygenize(pkg=myPackage, run=FALSE) #override dontrun
-  setwd(pa)
-}
+# if(Sys.getenv("USERNAME")%in%c("SQHW2","soren","sorhawell") || Sys.getenv("USER")=="travis2") {
+#   if(!interactive()) Sys.setenv(RGL_USE_NULL=TRUE) #disable RGL for headless machines
+#   library(devtools)
+#   library(tools)
+#   myPackage = "./forestFloor"
+#   
+#   #remake of devtools::run_examples
+#   run_examples_simple_no_Roxygenize = function(
+#     pkg = ".",
+#     topics = NULL,
+#     run = TRUE
+#   ) {
+#     pkg = as.package(pkg)
+#     if(is.null(topics)) {
+#       files = devtools:::rd_files(pkg)
+#     } else {
+#       files = unlist(lapply(topics,function(i) {
+#         fileName = devtools:::find_pkg_topic(topic=i)
+#         if(is.null(fileName)) stop(paste("no .Rd-file match found for[",i,"] when looking up topics"))
+#         return(fileName)
+#       }))
+#       path_man <- file.path(pkg$path, "man")
+#       files = paste0(path_man,"/",files)
+#       names(files) = basename(files)
+#     }
+#     lapply(files, devtools:::run_example,run=run)
+#     "Done"
+#   }
+#   
+#   #if a package check, jump out of test fold and into check source folder
+#   pa = getwd()
+#   if(!interactive()) setwd("../00_pkg_src") else setwd("../.")
+#   
+#   run_examples_simple_no_Roxygenize(pkg=myPackage, run=FALSE) #override dontrun
+#   setwd(pa)
+# }
