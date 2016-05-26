79b41463dc9ddbc60b7a5cfb2c8b69c1dad29384
sorhawell <sorhawell@gmail.com>
2016-05-26 14:14:28 +0200

    include test algorithm to confirm that gradient boosted trees would be compatible with forestFloor. simpleBoost is written as a wrapper around randomForest

---
+++ inst/examples/ffgradientBoost.R
@@ -0,0 +1,73 @@
+library(randomForest)
+library(forestFloor)
+
+#simulate data
+X     = data.frame(replicate(6,4*(runif(3000)-.5)))
+Xtest = data.frame(replicate(6,4*(runif(1500)-.5)))
+y     = with(X,X1^2+sin(X2*2*pi)+X3*X4) + rnorm(3000)/3
+ytest = with(Xtest,X1^2+sin(X2*6*pi)+X3*X4) + rnorm(3000)/3
+
+#define boosted tree wrapper
+predict.boostTree = function(Fx,X) {
+  class(Fx) = "randomForest"
+  predMatrix = predict(Fx,X,predict.all = T)$individual
+  ntrees = dim(predMatrix)[2]
+  return(apply(predMatrix,1,sum))
+}
+
+plot.boostTree = function(Fx,X,ytest,add=F,...) {
+  class(Fx) = "randomForest"
+  predMatrix = predict(Fx,X,predict.all = T)$individual
+  ntrees = dim(predMatrix)[2]
+  allPreds = apply(predMatrix,1,cumsum)
+  preds = apply(allPreds,1,function(pred) sd(ytest-pred))
+  if(add) plot=points
+  plot(1:ntrees,preds,...)
+  return()
+}
+
+simpleBoost = function(
+  X,y,    #training data
+  M=100,  #boosting iterations and ntrees
+  v=.1,   #learning rate
+  ...     #other parameters passed to randomForest
+) {
+  y_hat = y * 0  #latest ensemble prediction
+  res_hat = 0    #residuals hereof...
+  Fx = list()    #list for trees
+  for(m in 1:M) {
+    y_hat = y_hat + res_hat * v  #update prediction, by learning rate
+    res = y - y_hat              #compute residuals
+    hx = randomForest(X,res,ntree=1,keep.inbag=T,...) #grow tree on residuals
+    res_hat = predict(hx,X)                           #predict residuals
+    cat("SD=",sd(res),  "\n")
+    hx$forest$nodepred = hx$forest$nodepred * v
+    Fx[[m]] = hx
+  }
+  Fx = do.call(combine,Fx[])
+  Fx$y = y
+  Fx$oob.times = apply(Fx$inbag,1,function(x) sum(!x))
+  class(Fx) = c("boostTree","randomForest")
+  return(Fx)
+}
+
+#build gradient boosted forest
+rb = simpleBoost(X,y,M=300,replace=F,mtry=6,sampsize=500,v=0.005)
+
+#make forestFloor plots
+ffb = forestFloor(rb,X,Xtest)
+plot(ffb,plotTest=T,col=fcol(ffb,3,plotTest = TRUE))
+plot(ffb,plotTest=F,col=fcol(ffb,1,plotTest = FALSE))
+
+#validate model structure
+pred = predict(rb,X)
+predtest = predict(rb,Xtest)
+plot(y,pred,col="#00000034")
+plot(rb,Xtest,ytest,log="x")
+vec.plot(rb,X,i.var=1:2)
+
+#export plot
+png(file = "ffGradientBoost.png", bg = "transparent",width=800,height = 500)
+plot(ffb,plotTest=T,col=fcol(ffb,1))
+rect(1, 5, 3, 7, col = "white")
+dev.off()
