7dd032f6eff72458e1375980fcb6e0c6e394ac7e
Hajo Forsman <hajo.forsman@kpn.com>
2016-10-24 17:55:55 +0200

    small change to get Xtest columns in same order as X when rf.fit inherits randomforest.formula

--- R/forestFloor_source.R
+++ R/forestFloor_source.R
@@ -6,15 +6,18 @@ forestFloor = function(rf.fit,
                        bootstrapFC = FALSE,
                        ...) {
   Class = class(rf.fit)[1] #read only first class
-  
+
   #convert randomForest.formula
   if(inherits(rf.fit,"randomForest.formula")){
     X <- as.data.frame(X)
     rn <- row.names(X)
     Terms <- delete.response(rf.fit$terms)
     X <- model.frame(Terms, X, na.action = na.fail)
+    if(!is.null(Xtest)){
+      Xtest <- model.frame(Terms, Xtest, na.action = na.fail)
+    }
   }
-    
+
   #randomForest::randomForest or trimTrees::cinbag or rfPermute::rfPermute
   if(inherits(rf.fit,"randomForest")) {
     if(Class=="rfPermute") print("class 'rfPermute' supported as 'randomForest'")
@@ -24,12 +27,12 @@ forestFloor = function(rf.fit,
       if(!is.null(rf.fit$forest$nclass) && rf.fit$forest$nclass==2) {
         Type="regression"
       } else {
-        warning("binary_reg=T is not possible for >2 classes. 
+        warning("binary_reg=T is not possible for >2 classes.
                 Continue computation as multiClass")
-        
+
       }
     }
-  
+
     #dispatch either forestFloor_regression(and binary) or multiClassification
     switch(Type,
            regression = return(
@@ -54,15 +57,14 @@ forestFloor = function(rf.fit,
            ),#majorityTerminal
            stop("type of randomForest object is neither 'regression' or 'classification', (RF.fit$type==?)"))
   }
-  
+
   #other classes not supported...
   if(Class=="forestFloor_external") {
     print("forestFloor_external is a standardised treemodelfit which is not implemented yet")
     return("... cold emptyness")
   }
-  
+
   #if not classses recognized
   stop("This class is not yet supported, is this a random forest model fit?")
 
 }
-   
